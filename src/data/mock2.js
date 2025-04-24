26a01ae4f4c5f8f57301cde084223da6017196cc
AstroDude <sagittariusshaurya5@gmail.com>
2025-04-25 00:47:44 +0530

    added mock 2 templete

---
+++ src/data/mock2.js
@@ -0,0 +1,18 @@
+const mock2 = [
+  {
+    id: "q1",
+    questionText: "",
+    type: "text",
+    correctAnswer: "",
+    explanation: ``,
+  },
+  {
+    id: "q2",
+    questionText: "",
+    type: "text",
+    correctAnswer: "",
+    explanation: ``,
+  },
+];
+
+export default mock2;
