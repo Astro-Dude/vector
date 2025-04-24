fb5eb32b29651fa84c245a4f55469d52971a1c18
AstroDude <sagittariusshaurya5@gmail.com>
2025-04-25 01:28:57 +0530

    implemented vercel/Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from './context/ThemeContext'
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>,
 )
